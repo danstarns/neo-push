b308d0b53500cee269168f191144e61f88621fc8
Daniel Starns <danielstarns@hotmail.com>
2020-12-24 20:10:12 +0000

    feat: add post custom cypher and tests

--- packages/server/src/graphql/Post.ts
+++ packages/server/src/graphql/Post.ts
@@ -8,8 +8,34 @@ export const typeDefs = gql`
         blog: Blog @relationship(type: "HAS_POST", direction: "IN")
         comments: [Comment] @relationship(type: "HAS_COMMENT", direction: "OUT")
         author: User @relationship(type: "WROTE", direction: "IN")
-        isCreator: Boolean # TODO
-        isAuthor: Boolean # TODO
+        canEdit: Boolean
+            @cypher(
+                statement: """
+                OPTIONAL MATCH (this)<-[:WROTE]-(author:User {id: $jwt.sub})
+                OPTIONAL MATCH (this)<-[:HAS_POST]-(blog:Blog)
+                OPTIONAL MATCH (blog)<-[:HAS_BLOG]-(blogCreator:User {id: $jwt.sub})
+                OPTIONAL MATCH (blog)<-[:CAN_POST]-(blogAuthors:User {id: $jwt.sub})
+                WITH (
+                    (author IS NOT NULL) OR
+                    (blogCreator IS NOT NULL) OR
+                    (blogAuthors IS NOT NULL)
+                ) AS canEdit
+                RETURN canEdit
+                """
+            )
+        canDelete: Boolean
+            @cypher(
+                statement: """
+                OPTIONAL MATCH (this)<-[:WROTE]-(author:User {id: $jwt.sub})
+                OPTIONAL MATCH (this)<-[:HAS_POST]-(blog:Blog)
+                OPTIONAL MATCH (blog)<-[:HAS_BLOG]-(blogCreator:User {id: $jwt.sub})
+                WITH (
+                    (author IS NOT NULL) OR
+                    (blogCreator IS NOT NULL)
+                ) AS canDelete
+                RETURN canDelete
+                """
+            )
     }
 
     extend type Post
